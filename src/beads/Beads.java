1aaa602dcea833b5298182ca4991e45f49afbdd3
Sangeetha <pattaras@sheridancollege.ca>
2020-01-21 13:01:57 -0500

    First commit

---
+++ src/beads/Beads.java
@@ -0,0 +1,21 @@
+/*
+ * Sangeetha Pattara
+ * 991575884
+ * SYST10199 - Web Programming
+ */
+package beads;
+
+/**
+ *
+ * @author user
+ */
+public class Beads {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/beads/RoundBead.java
@@ -0,0 +1,14 @@
+/*
+ * Sangeetha Pattara
+ * 991575884
+ * SYST10199 - Web Programming
+ */
+package beads;
+
+/**
+ *
+ * @author user
+ */
+public class RoundBead {
+    
+}
